8c78725cb9b2eb7cee6682b468a3f6165afe9b8d
hoangxq <holmes.arther@gmail.com>
2023-11-11 00:56:29 +0700

    update api endpoint

--- src/app/commons/constants/api.ts
+++ src/app/commons/constants/api.ts
@@ -1,1 +1,2 @@
-export const ROOT_API: string = "http://localhost:8080/api";
+// export const ROOT_API: string = "http://localhost:8080/api";
+export const ROOT_API: string = "http://13.59.139.70/api";
